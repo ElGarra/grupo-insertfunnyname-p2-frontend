0a9409f0a65bb9b8ca0ee2b7fa570466c0f73288
Matias Duhalde <matias.duhalde@uc.cl>
2021-06-26 21:51:58 -0400

    feat: add base files for planned components

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import './App.css';
+import './App.scss';
+import Footer from '../Footer/Footer';
 
 function App() {
   return (
     <div className="App">
-      <h1>FindHomy</h1>
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Footer.scss';
+
+const Footer = () => (
+  <footer className="Footer">
+    <div className="element">
+      <h1>FindHomy</h1>
+    </div>
+  </footer>
+);
+
+export default Footer;
