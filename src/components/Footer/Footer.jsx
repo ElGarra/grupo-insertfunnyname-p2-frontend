e3407fe29395cac04185b2b14e086c7ab5459e9a
Felipe Guzman <faguzman2@uc.cl>
2021-06-27 22:13:04 -0400

    feat: add footer style

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,9 +3,30 @@ import './Footer.scss';
 
 const Footer = () => (
   <footer className="Footer">
-    <div className="element">
+    <div idName="logo-footer">
       <h1>FindHomy</h1>
+      <img src="https://img.wattpad.com/useravatar/Skereshrek.256.3927.jpg" alt="Logo" />
     </div>
+    <div className="footer-box">
+      <ul>
+        <li><a href="https://www.youtube.com/watch?v=5E2dsqjoFXg&ab_channel=JohnTimston">About</a></li>
+      </ul>
+      <hr />
+      <ul>
+        <li><a href="https://www.youtube.com/watch?v=pCjBetzC8ME&ab_channel=Meow%3FWoof%21">Contact</a></li>
+      </ul>
+      <hr />
+      <ul>
+        <li><a href="https://www.youtube.com/watch?v=GfT5CqrWJW0&ab_channel=ALIBA">Terms of Service</a></li>
+      </ul>
+    </div>
+    <div className="footer-box">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit.
+      Explicabo repudiandae eveniet iure provident suscipit minus
+      magnam facere, sit nihil veritatis. Dolorum illum ratione,
+      corrupti dolorem eum pariatur alias expedita ullam.
+    </div>
+    <div className="copyright">© Copyright 2021 InsertFunnyName</div>
   </footer>
 );
 
