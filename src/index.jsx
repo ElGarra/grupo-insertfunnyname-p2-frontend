8389e2d4b4d66978f8e79e690eba445fec809f71
Matias Duhalde <matias.duhalde@uc.cl>
2021-07-08 03:28:32 -0400

    feat: reorder scss index base styles

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.scss';
+import './styles/index.scss';
+import './styles/colors.scss';
+import './styles/typography.scss';
 import App from './App';
 
 ReactDOM.render(
