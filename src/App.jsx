ee022df0e83e268e9c411020c91fa3b804049f38
Matias Duhalde <matias.duhalde@uc.cl>
2021-07-08 04:17:01 -0400

    feat: Move footer and navbar inside router

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,9 @@
 import React from 'react';
-import Footer from './components/Footer/Footer';
 import Routes from './routes/Routes';
-import Navbar from './components/Navbar/Navbar';
 
 const App = () => (
   <div className="App">
-    <Navbar />
     <Routes />
-    <Footer />
   </div>
 );
 
--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Footer from '../components/Footer/Footer';
+import Navbar from '../components/Navbar/Navbar';
 
-import Home from '../views/Home/Home';
+import Home from '../views/Home';
 
 const Routes = () => (
   <Router>
+    <Navbar />
     <Route path="/" exact component={Home} />
+    <Footer />
   </Router>
 );
 
