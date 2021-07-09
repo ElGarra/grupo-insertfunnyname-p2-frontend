0c50df6f29101337cec75348b09ce2ccd3aedb6a
Matias Duhalde <matias.duhalde@uc.cl>
2021-07-09 04:52:41 -0400

    feat: do not throw error on some error codes

--- src/apis/backend.js
+++ src/apis/backend.js
@@ -2,6 +2,7 @@ import axios from 'axios';
 
 const backendAPI = axios.create({
   baseURL: process.env.REACT_APP_BACKEND_URL,
+  validateStatus: (status) => status < 500,
 });
 
 export default backendAPI;
