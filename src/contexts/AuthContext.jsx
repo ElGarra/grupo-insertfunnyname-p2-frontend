4c536643e1b054e2c1004bd94169e785d361103e
Matias Duhalde <matias.duhalde@uc.cl>
2021-07-09 21:54:04 -0400

    fix: replace useState for useLocalStorage hook

--- src/contexts/AuthContext.jsx
+++ src/contexts/AuthContext.jsx
@@ -1,18 +1,14 @@
-import React, {
-  createContext, //
-  useEffect,
-  useState,
-  useCallback,
-} from 'react';
+import React, { createContext, useEffect, useCallback } from 'react';
 import PropTypes from 'prop-types';
 import { useHistory } from 'react-router-dom';
 import jwtDecode from 'jwt-decode';
+import useLocalStorage from '../hooks/useLocalStorage';
 
 export const AuthContext = createContext();
 
 const AuthContextProvider = ({ children }) => {
-  const [currentUser, storeUser, clearStoredUser] = useState('user');
-  const [sessionExpDate, storeSessionExpDate, clearSessionExpDate] = useState('sessionExp');
+  const [currentUser, storeUser, clearStoredUser] = useLocalStorage('user');
+  const [sessionExpDate, storeSessionExpDate, clearSessionExpDate] = useLocalStorage('sessionExp');
   const history = useHistory();
   let logoutTimer;
 
