3f4312bc12388c334b316f287cf1641104f9dd0a
Matias Duhalde <matias.duhalde@uc.cl>
2021-07-10 03:00:54 -0400

    feat: add new components to views

--- src/views/Properties.jsx
+++ src/views/Properties.jsx
@@ -1,17 +1,34 @@
 import React from 'react';
 
-import IndexCard from '../components/IndexCard/IndexCard';
+import './Properties.scss';
+
+import IndexCard from '../components/Cards/IndexCard/IndexCard';
 
 const Properties = () => {
-  const text = 'Properties';
+  const tempProperty = {
+    imageLink: 'https://homeworlddesign.com/wp-content/uploads/2019/08/Stark-House-5-880x660.jpg',
+    title: 'Cool house',
+    street: 'Cool street',
+    streetNumber: 1337,
+    commune: 'Puerto Varas',
+    region: 'Los Lagos',
+    description: 'nice house',
+    bathrooms: 2,
+    bedrooms: 5,
+    size: 200,
+    type: 'house',
+    listingType: 'sale',
+    price: 220000,
+    createdAt: '12/12/2012',
+  };
   return (
     <div>
-      <h1>{text}</h1>
+      <h1>Properties</h1>
       <div className="cards-list">
-        <IndexCard />
-        <IndexCard />
-        <IndexCard />
-        <IndexCard />
+        <IndexCard property={tempProperty} />
+        <IndexCard property={tempProperty} />
+        <IndexCard property={tempProperty} />
+        <IndexCard property={tempProperty} />
       </div>
     </div>
   );
--- src/views/Property.jsx
+++ src/views/Property.jsx
@@ -1,16 +1,42 @@
 import React from 'react';
-import FullProperty from '../components/FullProperty/FullProperty';
-import CommentCard from '../components/CommentCard/CommentCard';
+
 import './Property.scss';
 
+import FullPropertyCard from '../components/Cards/FullPropertyCard/FullPropertyCard';
+import CommentCard from '../components/Cards/CommentCard/CommentCard';
+
 const Property = () => {
-  const text = 'Property';
+  const tempUser = {
+    firstName: 'Test',
+    lastName: 'User',
+    avatarLink: 'https://api.time.com/wp-content/uploads/2020/01/smudge-the-cat-interview.jpg',
+  };
+  const tempComment = {
+    body: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit.',
+    createdAt: '12/12/2012',
+  };
+  const tempProperty = {
+    imageLink: 'https://homeworlddesign.com/wp-content/uploads/2019/08/Stark-House-5-880x660.jpg',
+    title: 'Cool house',
+    street: 'Cool street',
+    streetNumber: 1337,
+    commune: 'Puerto Varas',
+    region: 'Los Lagos',
+    description: 'nice house',
+    bathrooms: 2,
+    bedrooms: 5,
+    size: 200,
+    type: 'house',
+    listingType: 'sale',
+    price: 220000,
+    createdAt: '12/12/2012',
+  };
   return (
     <div>
-      <h1>{text}</h1>
+      <h1>Property</h1>
       <div className="post-column">
-        <FullProperty />
-        <CommentCard />
+        <FullPropertyCard property={tempProperty} />
+        <CommentCard user={tempUser} comment={tempComment} />
       </div>
     </div>
   );
