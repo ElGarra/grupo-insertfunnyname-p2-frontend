fe3ee48848cba8cbfc94e7655c3a448df688cfdd
Matias Duhalde <matias.duhalde@uc.cl>
2021-07-10 04:25:41 -0400

    feat: protect profile view

--- src/views/Profile/Profile.jsx
+++ src/views/Profile/Profile.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import { Redirect } from 'react-router-dom';
+
+import useAuth from '../../hooks/useAuth';
 
 const Profile = () => {
+  const { currentUser } = useAuth();
+
   const text = 'Profile';
   return (
     <div>
+      {currentUser || <Redirect to="/" />}
       <h1>{text}</h1>
     </div>
   );
