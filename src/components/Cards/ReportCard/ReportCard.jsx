5859b6a6b9e6f4b179dd041cf71df8965f604954
Cata125 <31108920+Cata125@users.noreply.github.com>
2021-07-13 19:06:32 -0400

    Initial commit

---
+++ src/components/Cards/ReportCard/ReportCard.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import BaseCard from '../BaseCard/BaseCard';
+import './ReportCard.scss';
+
+function ReportCard(props) {
+  const { user } = props;
+  const username = `${user.firstName} ${user.lastName}`;
+  return (
+    <BaseCard>
+      <div className="ReportCard">
+        <div className="card__info">
+          <div className="card__info__user">
+            <img src={user.avatarLink} alt={`${username} Profile`} />
+            <h3>{username}</h3>
+          </div>
+        </div>
+        <div className="card__stats">
+          <p className="card__stats__title">E-mail</p>
+          <div className="card__stats__info">
+            <p>{user.email}</p>
+          </div>
+        </div>
+        <div className="card__stats">
+          <p className="card__stats__title">Joined</p>
+          <div className="card__stats__info">
+            <p>{`${new Date(user.createdAt).toLocaleDateString()}`}</p>
+          </div>
+        </div>
+      </div>
+    </BaseCard>
+  );
+}
+
+ProfileCard.propTypes = {
+  user: PropTypes.shape({
+    avatarLink: PropTypes.string.isRequired,
+    firstName: PropTypes.string.isRequired,
+    lastName: PropTypes.string.isRequired,
+    email: PropTypes.string.isRequired,
+    createdAt: PropTypes.string.isRequired,
+  }).isRequired,
+};
+
+export default ProfileCard;
---
+++ src/views/Admin/Admin.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import './Admin.scss';
+
+import BaseButton from '../../components/BaseButton/BaseButton';
+
+const Admin = () => {
+  const text = 'Reported users and comments';
+  return (
+    
+  );
+};
+
+export default Admin;
