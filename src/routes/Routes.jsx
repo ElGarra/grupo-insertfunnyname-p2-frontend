2b9a95ec1046cb1be9da3e7c61ec1ffe2e8fec74
Cata125 <31108920+Cata125@users.noreply.github.com>
2021-07-14 18:51:53 -0400

    feat: routes

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -12,6 +12,7 @@ import Signup from '../views/Signup/Signup';
 import AuthContextProvider from '../contexts/AuthContext';
 import Property from '../views/Property/Property';
 import Profile from '../views/Profile/Profile';
+import Admin from '../views/Admin/Admin';
 
 const Routes = () => (
   <Router>
@@ -25,6 +26,7 @@ const Routes = () => (
           <Route path="/properties" exact component={Properties} />
           <Route path="/properties/:propertyId" exact component={Property} />
           <Route path="/profile" exact component={Profile} />
+          <Route path="/admin" exact component={Admin} />
         </div>
       </div>
       <Footer />
