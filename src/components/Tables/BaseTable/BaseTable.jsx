883904bdf1cba01d6ef7b5bd702260abe652908f
Matias Duhalde <matias.duhalde@uc.cl>
2021-07-14 22:30:05 -0400

    feat: admin panel

--- src/components/Tables/BaseTable/BaseTable.jsx
+++ src/components/Tables/BaseTable/BaseTable.jsx
@@ -4,24 +4,41 @@ import PropTypes from 'prop-types';
 import './BaseTable.scss';
 
 const BaseTable = (props) => {
-  const { children, padding } = props;
+  const {
+    children, //
+    padding,
+    headers,
+    title,
+  } = props;
 
   return (
     <div className="wrapper">
-      <div className="BaseTable">
-        {padding ? <div className="table-padding">{children}</div> : children}
+      <h4 className="view-title">{title}</h4>
+      <div className={`BaseTable ${padding ? 'table-padding' : ''}`}>
+        <div className="row header">
+          {headers.map((header) => (
+            <div key={header} className="cell">
+              {header}
+            </div>
+          ))}
+        </div>
+        {children}
       </div>
     </div>
   );
 };
 
 BaseTable.propTypes = {
-  children: PropTypes.element.isRequired,
+  children: PropTypes.node,
   padding: PropTypes.bool,
+  headers: PropTypes.arrayOf(PropTypes.string).isRequired,
+  title: PropTypes.string,
 };
 
 BaseTable.defaultProps = {
+  children: null,
   padding: false,
+  title: '',
 };
 
 export default BaseTable;
--- src/views/Admin/Admin.jsx
+++ src/views/Admin/Admin.jsx
@@ -1,20 +1,29 @@
 import React, { useEffect, useState } from 'react';
+import { Redirect, useHistory } from 'react-router-dom';
+import jwtDecode from 'jwt-decode';
 
 import './Admin.scss';
 
+import useAuth from '../../hooks/useAuth';
 import apiClient from '../../apis/backend';
 import BaseButton from '../../components/BaseButton/BaseButton';
 import BaseTable from '../../components/Tables/BaseTable/BaseTable';
-import useAuth from '../../hooks/useAuth';
 
 const Admin = () => {
   const { currentUser } = useAuth();
+  const history = useHistory();
   const [userReports, setUserReports] = useState([]);
   const [commentReports, setCommentReports] = useState([]);
   const [loading, setLoading] = useState(false);
   const [message, setMessage] = useState('');
 
+  const userIsAdmin = currentUser && jwtDecode(currentUser).admin;
+
   useEffect(async () => {
+    if (!userIsAdmin) {
+      history.push('/');
+      return;
+    }
     try {
       setLoading(true);
       setMessage('');
@@ -36,78 +45,118 @@ const Admin = () => {
     }
   }, []);
 
+  const objTypes = {
+    comment: {
+      method: 'adminDeleteComment',
+      stateObj: commentReports,
+      setStateObj: setCommentReports,
+      args: ['propertyId', 'commentId'],
+      idField: 'commentId',
+    },
+    user: {
+      method: 'adminDeleteUser',
+      stateObj: userReports,
+      setStateObj: setUserReports,
+      idField: 'reportedUserId',
+    },
+  };
+
+  const deleteObject = (objType, report, args) => async () => {
+    const {
+      method, //
+      stateObj,
+      setStateObj,
+      idField,
+    } = objTypes[objType];
+    try {
+      setLoading(true);
+      setMessage('');
+      const response = await apiClient[method](...args, currentUser);
+      if (response.status !== 204) {
+        throw new Error();
+      }
+      setLoading(false);
+      const newState = stateObj.filter((obj) => obj[idField] !== report[idField]);
+      setStateObj(newState);
+    } catch (error) {
+      setMessage(`Could not delete ${objType}`);
+      setLoading(false);
+    }
+  };
+
   return (
-    <div>
-      <h1 className="view-title">User reports</h1>
+    <>
+      {userIsAdmin ? null : <Redirect to="/" />}
       {loading ? <p className="subtitle1">Loading...</p> : null}
       {message ? <p className="subtitle1">{message}</p> : null}
       {userReports ? (
-        <BaseTable>
-          <div className="row header">
-            <div className="cell">
-              UserId
-            </div>
-            <div className="cell">
-              ReportedUserId
-            </div>
-            <div className="cell">
-              Reason
-            </div>
-          </div>
-          {userReports.map((report) => (
-            <div className="row header">
-              <div className="cell">
-                {report.userId}
+        <BaseTable
+          title="User reports"
+          headers={[
+            'User ID',
+            'Reported User ID',
+            'Reason',
+            'Profile picture',
+            'Name',
+            'Email',
+            'Delete',
+          ]}
+        >
+          {userReports.map((report) => {
+            const reportedUserName = `${report.reportedUser.firstName} ${report.reportedUser.lastName}`;
+            return (
+              <div key={report.id} className="row">
+                <div className="cell">{report.userId}</div>
+                <div className="cell">{report.reportedUserId}</div>
+                <div className="cell">{report.reason}</div>
+                <div className="cell">
+                  <img src={report.reportedUser.avatarLink} alt={`${reportedUserName} Profile`} />
+                </div>
+                <div className="cell">{reportedUserName}</div>
+                <div className="cell">{report.reportedUser.email}</div>
+                <div className="cell">
+                  <BaseButton
+                    type="button"
+                    styleType="warning"
+                    onClick={deleteObject('user', report, [report.reportedUserId])}
+                  >
+                    Delete user
+                  </BaseButton>
+                </div>
               </div>
-              <div className="cell">
-                {report.reportedUserId}
-              </div>
-              <div className="cell">
-                {report.reason}
-              </div>
-            </div>
-          ))}
+            );
+          })}
         </BaseTable>
       ) : null}
 
-      <h1 className="view-title">Comment reports</h1>
-      {loading ? <p className="subtitle1">Loading...</p> : null}
-      {message ? <p className="subtitle1">{message}</p> : null}
-      {userReports ? (
-        <BaseTable>
-          <div className="row header">
-            <div className="cell">
-              UserId
-            </div>
-            <div className="cell">
-              CommentId
-            </div>
-            <div className="cell">
-              Reason
-            </div>
-            <div className="cell"> </div>
-          </div>
+      {commentReports ? (
+        <BaseTable
+          title="Comment reports"
+          headers={['User ID', 'Comment ID', 'Reason', 'Comment body', 'Delete']}
+        >
           {commentReports.map((report) => (
-            <div className="row header">
+            <div key={report.id} className="row">
+              <div className="cell">{report.userId}</div>
+              <div className="cell">{report.commentId}</div>
+              <div className="cell">{report.reason}</div>
+              <div className="cell">{report.Comment.body}</div>
               <div className="cell">
-                {report.userId}
-              </div>
-              <div className="cell">
-                {report.commentId}
-              </div>
-              <div className="cell">
-                {report.reason}
-              </div>
-              <div className="cell">
-                <BaseButton type="button">
-                  Edit property
+                <BaseButton
+                  type="button"
+                  styleType="warning"
+                  onClick={deleteObject('comment', report, [
+                    report.Comment.propertyId,
+                    report.commentId,
+                  ])}
+                >
+                  Delete comment
                 </BaseButton>
               </div>
             </div>
           ))}
         </BaseTable>
       ) : null}
-    </div>
+    </>
   );
 };
 
